4c6aad4e6a493adffc0a6d19a067acf8b0562139
izvol <v.izvol@iteco-inno.ru>
2022-03-13 12:10:23 +0500

    Add name variable

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, GITHUB!");
+
+        String name = "Izvol";
+        System.out.println(name);
+
+
     }
 }
